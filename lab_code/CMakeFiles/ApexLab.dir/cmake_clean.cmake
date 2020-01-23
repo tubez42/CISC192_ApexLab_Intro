abbef428e673bad8c19cfb1bd8c0771973de0ec2
SDMesaAssignments <amilner001@sdccd.edu>
2020-01-23 23:16:38 +0000

    Run script test

---
+++ lab_code/CMakeFiles/ApexLab.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ApexLab.dir/testbench.cpp.o"
+  "CMakeFiles/ApexLab.dir/apex_code.cpp.o"
+  "ApexLab.pdb"
+  "ApexLab"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ApexLab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Run script for repl.it
 cd lab_code && cmake CMakeLists.txt && make && ./ApexLab
+
+# End scriptls -
