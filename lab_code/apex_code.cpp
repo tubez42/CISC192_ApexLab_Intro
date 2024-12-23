f0f3c1ab44488da41531a5527087fe9b29db95e5
kfoksdccd <126300842+kfoksdccd@users.noreply.github.com>
2024-12-23 14:57:56 -0800

    Update apex_code.cpp

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -1,12 +1,12 @@
 //
+// Created by amilner001 on 8/3/2019.
 //
 #include <iostream>
 #include "apex_code.h"
 
 using namespace std;
 
-void quiz(stringstream& cin, stringstream& cout)
-{
-    
+void print_my_name()
+{   
+    cout << "Hello C++! I'm <put_name_here" << endl;
 }
-
