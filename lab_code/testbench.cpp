70f809aa735ff39c016958a53a783fa3ce9fdc65
kfoksdccd <126300842+kfoksdccd@users.noreply.github.com>
2024-12-23 14:56:34 -0800

    Update testbench.cpp

--- lab_code/testbench.cpp
+++ lab_code/testbench.cpp
@@ -1,37 +1,6 @@
-//
-// Don't modify code in this file, with the exception
-// of adding test cases.
-//
-#include <iostream>
-#include <fstream>
 #include "apex_code.h"
 
-using namespace std;
-
-#define TEST_FUNC_PTR void (*test_func)(stringstream&, stringstream&)
-void test(TEST_FUNC_PTR, const string& input, const string& expected)
-{
-    stringstream in_stream(input);
-    stringstream out_stream;
-    test_func(in_stream, out_stream);
-
-    if(expected == out_stream.str())
-    {
-        cout << "PASS" << endl;
-    }
-    else
-    {
-        cout << "With input \"" << input << "\" expected output \"" << expected
-             << "\", received \"" << out_stream.str() << "\"" << endl;
-    }
-}
-
-int main()
-{
-    // To add a test case, duplicate one of the test lines below
-    // modify input with the first string, and the expected output
-    // in the second string.
-
-
+int main() {
+    print_my_name();
     return 0;
 }
